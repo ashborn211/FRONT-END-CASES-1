576c87b1cf9d51808de63b1349b07168f367e4db
velican <1201396@student.roc-nijmegen.nl>
2024-03-11 15:03:42 +0100

    Update BarChart.jsx

--- src/BarChart.jsx
+++ src/BarChart.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import "./BarChart.css";
 
 const BarChart = () => {
+  const bars = [
+    { height: "12%", label: "label1" },
+    { height: "16%", label: "label2" },
+    { height: "22%", label: "label3" },
+    { height: "30%", label: "label4" },
+    { height: "45%", label: "label5" },
+    { height: "60%", label: "label6" },
+  ];
   return (
     <>
-        <div className="barcontainer">
+      {/* <div className="barcontainer">
           <div className="bar" style={{ height: "12%" }}>
             bar
             <div className="barlabel">label</div>
@@ -29,7 +37,15 @@ const BarChart = () => {
             bar
             <div className="barlabel">label</div>
           </div>
-        </div>
+        </div> */}
+      <div className="barcontainer">
+        {bars.map((bar, index) => (
+          <div key={index} className="bar" style={{ height: bar.height }}>
+            bar
+            <div className="barlabel">{bar.label}</div>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
