912cdc2428c796f33444e6a80cb78d0e006d96bd
velican <120579492+ashborn211@users.noreply.github.com>
2024-03-12 11:43:53 +0100

    test grafiek met JS heb nog CSS nodig. chart.js werkt niet

--- src/BarChart.jsx
+++ src/BarChart.jsx
@@ -1,11 +1,45 @@
 import React from "react";
 import "./BarChart.css";
+import {useEffect} from 'react'
+
 
 const BarChart = () => {
 
+  useEffect(() => {
+
+    const data = [10, 20, 15, 25];
+
+    // Container voor de grafiek
+    const chartContainer = document.getElementById("bar-chart");
+    chartContainer.innerHTML = "";
+
+    // maak een bar voor elk ding
+    data.forEach((value, index) => {
+      const bar = document.createElement("div");
+      bar.className = "bar";
+      bar.style.height = `${value * 5}px`;
+      bar.style.backgroundColor = getRandomColor();
+      chartContainer.appendChild(bar);
+    });
+  }, []);
+
+  //maak random kleur voor de grafiek
+  const getRandomColor = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
   return (
     <>
-
+      <div className="data-container">
+        <div className="data-box">
+          <div id="bar-chart"></div>
+        </div>
+      </div>
     </>
   );
 };
