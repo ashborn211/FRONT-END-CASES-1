948333d2f255d6b224fa476c9ff0dc29749f1d76
velican <120579492+ashborn211@users.noreply.github.com>
2024-03-26 12:09:07 +0100

    niks

--- src/BarChart.jsx
+++ src/BarChart.jsx
@@ -5,37 +5,37 @@ import axios from 'axios';
 
 const BarChart = () => {
   const [fetchCryptoList, setfetchCrypto] = useState([]);
+  const [barHeight, setBarHeight] = useState(0); // State variable to store bar height
   const fetchCrypto = () => {
     axios
       .get("https://api.coincap.io/v2/assets")
       .then(function (response) {
-        const data = response.data.results;
+        const data = response.data.data.priceUsd;
+        setBarHeight(response.data.data[0].priceUsd);
         setfetchCrypto(data);
         console.log(response);
       }).catch(error => {
         console.error(error);
       });
   };
-
   useEffect(() => {
     fetchCrypto();
+
     //voor het testen
-    const data = [10, 20, 15, 25];
+    const bars = [1, 2, 3, 4, 5, 6, 7, 8];
 
     // Container voor de grafiek
     const chartContainer = document.getElementById("bar-chart");
     chartContainer.innerHTML = "";
 
     // maak een bar voor elk ding
-    data.forEach((value) => {
+    bars.forEach(() => {
       const bar = document.createElement("div");
       bar.className = "bar";
-      bar.style.height = `${value * 5}px`;
+      bar.style.height = `${barHeight/1000}%`; // Set the bar height dynamically
       chartContainer.appendChild(bar);
     });
-  }, []);
-
-
+  }, [barHeight]); // Include barHeight in the dependency array to re-run the effect when it changes
 
   return (
     <>
