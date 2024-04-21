d7793e0b25379773a6e9623c50879acdd2a6b57a
velican <1201396@student.roc-nijmegen.nl>
2024-04-21 21:25:59 +0200

    Update LineChart.jsx

--- src/LineChart.jsx
+++ src/LineChart.jsx
@@ -18,7 +18,7 @@ function LineChart() {
   };
 
   const formatDate = (timestamp) => {
-    const date = new Date(timestamp * 1000); // convert timestamp to milliseconds
+    const date = new Date(timestamp * 1000); // naar milliseconden
     const day = String(date.getDate()).padStart(2, "0");
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const year = date.getFullYear();
